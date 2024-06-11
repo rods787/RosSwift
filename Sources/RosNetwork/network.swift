da620da094fee5fbfa59f5866be67c72a3541462
rods787 <respruel@liberty.edu>
2024-06-10 21:15:59 -0400

    Update network.swift

--- Sources/RosNetwork/network.swift
+++ Sources/RosNetwork/network.swift
@@ -35,7 +35,7 @@ public struct RosNetwork {
         //} else if 
         let it = remappings["__ip"] {
             host = it
-        //}
+        }
         if let it = remappings["__tcpros_server_port"] {
             guard let tcprosServerPort = UInt16(it) else {
                 fatalError("__tcpros_server_port [\(it)] was not specified as a number within the 0-65535 range")
